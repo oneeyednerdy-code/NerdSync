821e524e5cf8e7eb8c75ef89cbde6764b78f9fe8
oneeyednerdy-code <oneeyednerdy@gmail.com>
2026-08-19 16:21:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuration File for Nerd Sync Twitch Dashboard
 // Replace the placeholder string below with your Twitch Application Client ID.
 const CONFIG = {
-  TWITCH_CLIENT_ID: "YOUR_TWITCH_CLIENT_ID_HERE"
+  TWITCH_CLIENT_ID: "0kvyl010kornp9v8a5lwtein3ftmu1"
 };
